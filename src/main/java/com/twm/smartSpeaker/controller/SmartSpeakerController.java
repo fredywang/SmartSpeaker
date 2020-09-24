27aaf940ffa717ac67c68be6ade755c2597e79be
fredywang <fredywang2005@gmail.com>
2020-09-24 20:43:29 +0800

    update

--- src/main/java/com/twm/smartSpeaker/controller/SmartSpeakerController.java
+++ src/main/java/com/twm/smartSpeaker/controller/SmartSpeakerController.java
@@ -19,7 +19,7 @@ public class SmartSpeakerController {
 	private static final Logger logger = LoggerFactory.getLogger(SmartSpeakerController.class);
 	
 	@RequestMapping(value="getOAuth")
-	public String getOAuth(HttpServletRequest request,@RequestParam(value="id") String id) {
+	public String getOAuth(HttpServletRequest request,@RequestParam(value="id", required = false) String id) {
 		String r = "id="+id+",連線成功";
 		logger.info(r);
 		return r;
